642af7f620b581b09547ba558cee470f65a283d9
Victor <victor.chicinas@gmail.com>
2024-08-14 14:15:40 +0200

    Passes current tests

--- csharp-tdd-bobs-bagels.tests/UnitTest1.cs
+++ csharp-tdd-bobs-bagels.tests/UnitTest1.cs
@@ -1,3 +1,5 @@
+using tdd_bobs_bagels.CSharp.Main;
+
 namespace csharp_tdd_bobs_bagels.tests;
 
 public class Tests
@@ -8,8 +10,81 @@ public class Tests
     }
 
     [Test]
-    public void Test1()
+    public void addOneBagel_1()
     {
-        Assert.Pass();
+        Core core = new Core();
+        Assert.True(core.addBagle(Core.BagleType.PLAINBAGEL));
     }
+
+    [Test]
+    public void addOneBagel_2()
+    {
+        Core core = new Core();
+        core.addBagle(Core.BagleType.PLAINBAGEL);
+        Assert.True(core.addBagle(Core.BagleType.PLAINBAGEL));
+    }
+
+    [Test]
+    public void removeOneBagel_1()
+    {
+        Core core = new Core();
+        core.addBagle(Core.BagleType.PLAINBAGEL);
+        Assert.True(core.removeBagle(Core.BagleType.PLAINBAGEL));
+    }
+
+    [Test]
+    public void removeOneBagel_2()
+    {
+        Core core = new Core();
+        Assert.False(core.removeBagle(Core.BagleType.PLAINBAGEL));
+    }
+
+    [Test]
+    public void assertCurrentSize_1()
+    {
+        Core core = new Core();
+        core.addBagle(Core.BagleType.PLAINBAGEL);
+        core.addBagle(Core.BagleType.VICTORSSPECIALBAGEL);
+        core.addBagle(Core.BagleType.PLAINBAGEL);
+        core.addBagle(Core.BagleType.PLAINBAGEL);
+        core.addBagle(Core.BagleType.VICTORSSPECIALBAGEL);
+        Assert.That(core.getCurrentBasketSize() == 5);
+    }
+
+    [Test]
+    public void assertCurrentSize_2()
+    {
+        Core core = new Core();
+        core.addBagle(Core.BagleType.PLAINBAGEL);
+        core.addBagle(Core.BagleType.VICTORSSPECIALBAGEL);
+        core.addBagle(Core.BagleType.PLAINBAGEL);
+        core.addBagle(Core.BagleType.PLAINBAGEL);
+        core.addBagle(Core.BagleType.VICTORSSPECIALBAGEL);
+        core.addBagle(Core.BagleType.VICTORSSPECIALBAGEL);
+        Assert.False(core.getCurrentBasketSize() == 6);
+    }
+
+    [Test]
+    public void assertCurrentSize_3()
+    {
+        Core core = new Core();
+        core.addBagle(Core.BagleType.PLAINBAGEL);
+        core.addBagle(Core.BagleType.VICTORSSPECIALBAGEL);
+        core.addBagle(Core.BagleType.PLAINBAGEL);
+        core.addBagle(Core.BagleType.PLAINBAGEL);
+        core.addBagle(Core.BagleType.VICTORSSPECIALBAGEL);
+        core.addBagle(Core.BagleType.VICTORSSPECIALBAGEL);
+        core.removeBagle(Core.BagleType.PLAINBAGEL);
+        Assert.That(core.getCurrentBasketSize() == 4);
+    }
+
+    [Test]
+    public void assertMaxSize_1()
+    {
+        Core core = new Core();
+        core.setBasketSize(10);
+        Assert.That(core.getBasketSize() == 10);
+    }
+
+
 }
--- tdd-bobs-bagels.CSharp.Main/Core.cs
+++ tdd-bobs-bagels.CSharp.Main/Core.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,79 @@ namespace tdd_bobs_bagels.CSharp.Main
 {
     public class Core
     {
+
+        /// <summary>
+        /// enum as my bagle shop only produces standardised bagles.
+        /// </summary>
+        public enum BagleType
+        {
+            PLAINBAGEL,
+            EGGBAGEL,
+            RAINBOWBAGEL,
+            VICTORSSPECIALBAGEL,
+            CUSTOMBAGLE
+        }
+
+
+        /// <summary>
+        /// Shoppinglist
+        /// </summary>
+        private List<BagleType> bagleBasket;
+
+        //Max number of items in baset
+        private int basketSize;
+
+        //Corrent number of items in basket
+        private int currentBasketSize;
+
+        /// <summary>
+        /// Init all bagle-related data
+        /// </summary>
+        public Core()
+        {
+
+            bagleBasket = new List<BagleType>();
+            basketSize = 5;
+            currentBasketSize = 0;
+        }
+
+        /// <summary>
+        /// adds bagle if 
+        /// </summary>
+        /// <param name="b"></param>
+        /// <returns></returns>
+
+        public bool addBagle(BagleType b)
+        {
+            if (basketSize <= currentBasketSize)
+            {
+                return false;
+            }
+            else
+            {
+                bagleBasket.Add(b);
+                currentBasketSize++;
+                return true;
+            }
+        }
+
+        public bool removeBagle(BagleType b)
+        {
+            if (bagleBasket.Remove(b))
+            {
+                currentBasketSize--;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public int getBasketSize() { return basketSize; }
+        public int getCurrentBasketSize() { return currentBasketSize; }
+
+        public void setBasketSize(int newBasketSize) { basketSize = newBasketSize; }
+
     }
 }
